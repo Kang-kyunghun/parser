0c8a7c9fe7016d8446195611a349b6dee16a68a0
Jack-earlysloth <kgh239@earlysloth.com>
2020-11-06 14:30:28 +0900

    mapper 기본구조 구현

--- mapper/request_data.py
+++ mapper/request_data.py
@@ -1,4 +1,30 @@
 
+# request_data_radio = {
+#     "surveyId": "779442",
+#     "urlToken": "rs7mFWc",
+#     "version": 2,
+#     "contents" : {
+#       "survey_title": "모든 타입 설문",
+#       "survey_description": "모든 타입 설문",
+#       "googleFormResponseRemoteKey": "엑셀 파일의 s3 remote key",
+#       "body": [
+#             {
+#                 "type": "radio",
+#                 "title": "현재 하고 있는 일은?",
+#                 "body": [
+#                 "브랜디",
+#                 "머치스퀘어",
+#                 "리베라비트",
+#                 "위고두팀",
+#                 "기타:"
+#                 ],
+#                 "url": "https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/uu/6f00627a-60ca-4d8b-8989-18589e9a6eb1.png",
+#                 "isrequired": True
+#             }]
+#         }   
+# }
+
+
 request_data_radio = {
     "surveyId": "779442",
     "urlToken": "rs7mFWc",
@@ -8,6 +34,15 @@ request_data_radio = {
       "survey_description": "모든 타입 설문",
       "googleFormResponseRemoteKey": "엑셀 파일의 s3 remote key",
       "body": [
+            {
+                "type": "shorttext",
+                "title": "기수를 입력해주세요",
+                "body": "",
+                "url": "",
+                "isrequired": True,
+                "subType": "text",
+                
+            },
             {
                 "type": "radio",
                 "title": "현재 하고 있는 일은?",
@@ -20,7 +55,26 @@ request_data_radio = {
                 ],
                 "url": "https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/uu/6f00627a-60ca-4d8b-8989-18589e9a6eb1.png",
                 "isrequired": True
-            }]
+            },
+            {
+                "type": "radio_image_selections",
+                "title": "현재 기분과 가장 비슷한 것은?",
+                "body": [
+                  "졸림",
+                  "쉬고 싶음",
+                  "자신감",
+                  "기타:"
+                ],
+                "url": "",
+                "isrequired": False,
+                "image_selections": [
+                  "https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/uu/3b96c893-52f5-4fa2-85f3-97e42e759e09.jpeg",
+                  "https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/uu/7bca68a9-f2c6-4233-87ea-6ffe0f7c0dd4.jpeg",
+                  "https://s3.ap-northeast-2.amazonaws.com/pocketsurvey.earlysloth/images/uu/7bca68a9-f2c6-4233-87ea-6ffe0f7c0dd4.jpeg"
+
+                ],
+            },
+            ]
         }   
 }
 
--- mapper/test_code.py
+++ mapper/test_code.py
@@ -1,13 +1,36 @@
 import request_data
 import pandas as pd
+from math import isnan
+from uuid import uuid4
 
-def mapper_radio(blueprint_contents, data_excel):
-    
+# len(data_excel) :  row의 길이
+# len(data_excel.columns) :column의 길이
+# data_excel.values[row] : 1명이 답한 모든 답
+def mapper_radio(data_request, data_excel, changed_time, uuid):
+    print('mapper_radio')
+    return {}
+    # answers = []
+    # for row in range(len(data_excel)):
+    #     val = data_excel.values[row]
+    #     # print(val)
+    #     answers.append(val)
+    # print(answers[0][0])
+    # blank = answers[-1][-1]
+    # if isnan(blank):
+    #     print('value is none')
+    # print(type(blank))
 
-    titles = list(data_excel.columns)
-    for row in range(len(data_excel)):
-        val = str(data_excel.values[row][2])
-        print(val)
+def mapper_shorttext(data_request, data_excel, changed_time, uuid):
+    print('mapper_shorttext')
+    return {}
+
+def mapper_radio_image_selections(data_request, data_excel, changed_time, uuid):
+    print('mapper_radio_image_selections')
+    return {}
+
+def change_time_format(local_time):
+    return 1111
+
 
 
 
@@ -34,8 +57,49 @@ def mapper_radio(blueprint_contents, data_excel):
 
 
 if __name__ == '__main__':
-    data = request_data.request_data_radio
-    data_contents = data['contents']
-    FILE_PATH = 'test_data.xlsx'
+    data_request = request_data.request_data_radio
+    data_content = data_request['contents']
+    FILE_PATH = './test_code_data.xlsx'
     data_excel = pd.read_excel(FILE_PATH)
-    mapper_radio(data_contents, data_excel)
+    #설문 전체 문항들 제목 list
+    titles = list(data_excel.columns)
+    type_dict = {}
+    response_data = []
+    uuid = str(uuid4())
+    for question in data_content['body']:
+        for index in range(1,len(titles)):
+            if titles[index] == question['title']:
+                type_dict[index] = question['type']
+                break
+
+    # len(data_excel) :  row의 길이
+    # len(data_excel.columns) :column의 길이
+    # data_excel.values[row] : 1명이 답한 모든 답
+    answers_all = data_excel.values
+    for ansewrs_person in answers_all:
+        changed_time = change_time_format(ansewrs_person[0])
+        for index in range(1, len(ansewrs_person)):
+            data_request = ansewrs_person[index]
+            question_type = type_dict[index]
+            if question_type == 'radio':
+                question_data = mapper_radio(data_request, data_excel, changed_time, uuid)
+                response_data.append(question_data)
+            elif question_type == 'shorttext':
+                question_data = mapper_shorttext(data_request, data_excel, changed_time, uuid)
+                response_data.append(question_data)
+            elif question_type == 'radio_image_selections':
+                question_data = mapper_radio_image_selections(data_request, data_excel, changed_time, uuid)
+                response_data.append(question_data)
+    result = {
+        "responseData" : response_data,
+        "surveyId"     : data_request["surveyId"],
+        "urlToken"     : data_request["urlToken"],
+        "uuid"         : uuid,
+        "version"      : data_request["version"]
+        
+    }
+        
+    
+    # test_dict ={'type' : mapper_radio(data, data_excel)}
+    # if data_type == 'radio':
+    # mapper_radio(data, data_excel)
